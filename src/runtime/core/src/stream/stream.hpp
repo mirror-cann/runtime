57b88a40ed53671a3f77f44ddd3440336454f6ea
zhangxinxing_xx <zhangxingxing33@hisilicon.com>
2026-06-10 19:29:09 +0800

    回退溢出检测功能
    Co-authored-by: zhangxingxing33@hisilicon.com<zhangxingxing33@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2725 merge 610 into master
    
    回退溢出检测功能
    
    Created-by: zhangxinxing_xx
    Commit-by: zhangxingxing33@hisilicon.com
    Merged-by: cann-robot
    Description: 
    # Pull Request
    
    ## 描述
    请清晰准确地描述本次 Pull Request 的意图和变更内容。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.
    2.
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [ ] 我的代码遵循了项目的代码风格
    - [ ] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [ ] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [ ] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    See merge request: cann/runtime!2725

--- src/runtime/core/src/stream/stream.hpp
+++ src/runtime/core/src/stream/stream.hpp
@@ -709,9 +709,6 @@ public:
 
     bool IsOverflowEnable(void) const
     {
-        if (satMode_ == RT_OVERFLOW_MODE_INFNAN) {
-            return true;
-        }
         return (isDebugRegister_ || isOverflowSwitchOn_);
     }
 
