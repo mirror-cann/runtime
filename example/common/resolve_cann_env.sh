51f4c7139ceb6814da9c07f9f75d16e07c9d333b
Reyn52166 <leihuan1@huawei.com>
2026-06-23 15:01:32 +0800

    【PR】: resolve_cann_env.sh增加"set -e"
    Co-authored-by: Reyn52166<leihuan1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3041 merge master into master
    
    【PR】: resolve_cann_env.sh增加"set -e"
    
    Created-by: Reyn52166
    Commit-by: Reyn52166
    Merged-by: cann-robot
    Description: 
    # Pull Request
    
    ## 描述
    resolve_cann_env.sh增加"set -e"
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    NA
    
    
    ## 如何测试
    不涉及功能
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    See merge request: cann/runtime!3041

--- example/common/resolve_cann_env.sh
+++ example/common/resolve_cann_env.sh
@@ -9,6 +9,8 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # -----------------------------------------------------------------------------------------------------------
 
+set -e
+
 resolve_cann_arch_dir() {
     local machine
 
